6e620500c4abc164d8d9b482142bb47aac64bc64
MrMFWIC <Benstar02@hotmail.ca>
2023-10-19 16:52:25 -0400

    Added Animated Main Menu, Added End Screen, Added Player Jump, Added Level Destruction

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 5;
-    public float leftRightSpeed = 4;
+    public float leftRightSpeed = 5;
     static public bool canMove = false;
+    public bool isJumping = false;
+    public bool comingDown = false;
+    public GameObject playerObject;
 
     void Update()
     {
@@ -29,6 +32,38 @@ public class PlayerMove : MonoBehaviour
                     transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
                 }
             }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
+            {
+                if (isJumping == false)
+                {
+                    isJumping = true;
+                    playerObject.GetComponent<Animator>().Play("Jump");
+                    StartCoroutine(JumpSequence());
+                }
+            }
         }
+
+        if (isJumping == true)
+        {
+            if (comingDown == false)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 4, Space.World);
+            }
+            else
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * -4, Space.World);
+            }
+        }
+    }
+
+    IEnumerator JumpSequence()
+    {
+        yield return new WaitForSeconds(0.45f);
+        comingDown = true;
+        yield return new WaitForSeconds(0.45f);
+        isJumping = false;
+        comingDown = false;
+        playerObject.GetComponent<Animator>().Play("Standard Run");
     }
 }
